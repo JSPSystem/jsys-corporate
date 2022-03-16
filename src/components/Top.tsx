4bc7a1ea2bd31339b601098e2c8cbd6a836010ca
JSPSystem <84693166+JSPSystem@users.noreply.github.com>
2022-03-17 07:31:59 +0900

    共通の背景色とTOPのコメントアウトを削除

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -36,13 +36,6 @@ const Top = () => {
       });
   }, []);
 
-  /*
-  <article
-      id="content_top"
-      className="relative flex justify-center items-center w-full h-128 md:h-screen bg-cover bg-center bg-img-top-mb md:bg-img-top font-jura text-white"
-    ></article>
-  */
-
   return (
     <article
       id="content_top"
